d87756745a1e90935c97d46223bb12e7d57f6c7a
rAjendrAprAsAd-embedded <58771299+rAjendrAprAsAd-embedded@users.noreply.github.com>
2020-03-24 17:22:30 +0530

    words reverse in a string
    i/p my name is groot 
    o/p ym eman si toorg

---
+++ string_substring_reverse.c
@@ -0,0 +1,40 @@
+#include <stdio.h> 
+#include <string.h> 
+  
+void printReverse(char arr[]) 
+{ 
+	char temp;
+    int length = strlen(arr); 
+  printf("%ld\n",length);
+    
+    int i=0,k,dummy; 
+    for (i = 0;i<=length;)
+	 {        
+		k=i;
+ 	 while(arr[k]!= ' ' && k!=length)
+	 {
+        	k++;
+        } 
+	
+	dummy=i;i=k+1;
+	k--;
+	while(dummy<k)
+	{
+		temp=arr[k];
+		arr[k]=arr[dummy];
+		arr[dummy]=temp;	
+	dummy++;k--;
+
+	}
+	
+    } 
+  
+    printf("%s", arr); 
+} 
+  
+int main() 
+{ 
+    char str[] = "Raj AM A GEEK"; 
+    printReverse(str); 
+    return 0; 
+} 
